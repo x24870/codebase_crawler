b07bc04f8794de105a325f1c09f8dfcfd847d68e
pyk <x24870@gmail.com>
2019-11-05 19:33:42 +0800

    Able to login

--- get_ssh_link.py
+++ get_ssh_link.py
@@ -5,9 +5,18 @@ from urllib.request import urlopen
 
 AMI_GIT_URL = "https://megaracgit.ami.com"
 
-def get_all_repository_link():
-	resp = urlopen(AMI_GIT_URL)
-	html = BeautifulSoup(resp)
-	print(html)
+cookies = {'__cfduid': 'd0269b8f63706340b350b4f38e06558301542807752',
+'_gitlab_session': '21bbafaeb6b082a6642288c249ab074c',
+'experimentation_subject_id': 'IjM2MDY5OTFiLTJjZjItNGYzNC05M2QyLTQ0M2U2MGU2MTI4MyI%3D--b7ecf780b7ab92f8190d72985743604a837789a6'}
 
-get_all_repository_link()
+def get_all_repository_link(url):
+	# resp = urlopen(url)
+	# html = BeautifulSoup(resp)
+	# print(html)
+	resp = requests.get(url, cookies=cookies)
+	resp.raise_for_status()
+	soup = BeautifulSoup(resp.text, 'html5lib')
+	print(resp.text)
+	#return soup
+
+get_all_repository_link(AMI_GIT_URL)
