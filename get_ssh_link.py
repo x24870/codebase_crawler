00ee57a8f75b79bb97ac84edff170fb8c47912f1
pyk <x24870@gmail.com>
2019-11-08 14:56:33 +0800

    Able to clone codebase

--- get_ssh_link.py
+++ get_ssh_link.py
@@ -1,6 +1,7 @@
-import os, requests, datetime
+import os, requests, datetime, subprocess
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import git
 
 ### URL pattern
 # https://megaracgit.ami.com/?non_archived=true&page=2&sort=created_desc
@@ -10,6 +11,7 @@ page = 'page='
 sort = 'sort=created_desc'
 
 REC_PATH = 'record'
+DOWNLOAD_PATH = 'download'
 
 cookies = {'__cfduid': 'd0269b8f63706340b350b4f38e06558301542807752',
 '_gitlab_session': '21bbafaeb6b082a6642288c249ab074c',
@@ -18,12 +20,13 @@ cookies = {'__cfduid': 'd0269b8f63706340b350b4f38e06558301542807752',
 def initial():
 	if not os.path.exists(REC_PATH):
 		os.makedirs(REC_PATH)
+	if not os.path.exists(DOWNLOAD_PATH):
+		os.makedirs(DOWNLOAD_PATH)
 
 def get_record_path():
 	date = datetime.datetime.now().strftime("%Y%m%d")
 	return os.path.join(REC_PATH, date+'.txt')
 	
-
 def check_next_page(soup):
 	e_next_btn = soup.find('a', {'rel': 'next'})
 	if e_next_btn['href'] == '#':
@@ -55,7 +58,7 @@ def get_page_repository_link(url):
 def save_all_link(f_path):
 	https_link_lst = []
 
-	for page_num in range(5, 10):#TODO: change to 1
+	for page_num in range(1, 10):#TODO: modify range to crawl different pages
 		print('--------------------- Page {} ---------------------'.format(page_num))
 		url = '{}/?{}&{}{}&{}'.format(AMI_GIT_URL, non_arch, page, page_num, sort)
 		print('URL: ' + url)
@@ -69,16 +72,24 @@ def save_all_link(f_path):
 	print('*** Saved all clone https links successully!: '.format(f_path))
 	
 
-def clone_all_repositiry(f_path):
+def clone_all_repositiry(record_path):
+	### https url pattern
+	# https:USERNAME:PASSWORD//megaracgit.ami.com/tools/mds/releases/12.0.0.git
+
 	url_lst = []
-	with open(f_path, 'r') as f:
+	with open(record_path, 'r') as f:
 		url_lst = f.readlines()
-	print(url_lst)
+	for url in url_lst:
+		url = url.replace('megaracgit', 'RickYe:Pegaaccount0@megaracgit')
+		clone_path = os.path.join( DOWNLOAD_PATH, os.path.basename(url).split('.git')[0] )
+		print("URL: " + url)
+		print("PATH: " + clone_path)
+		subprocess.call(['git', 'clone', url, clone_path])
 
 if __name__ == '__main__':
 	initial()
 	save_all_link( get_record_path() )
-	clone_all_repositiry( get_record_path() )
+	#clone_all_repositiry( get_record_path() )
 
 		
 	
